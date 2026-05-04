8a39c5d12f9ae2a19f1aa53a9e96739d7cc09a39
Theo Champion <tchampio@c2r12p3.42lehavre.fr>
2026-05-04 19:42:10 +0200

    feat: flags arguments are fully handled now

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tchampio <tchampio@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 14:49:59 by tchampio          #+#    #+#             */
-/*   Updated: 2026/05/04 18:43:17 by tchampio         ###   ########.fr       */
+/*   Updated: 2026/05/04 19:40:54 by tchampio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,43 @@
 #include "types.h"
 #include <stdlib.h>
 #include <stdbool.h>
+
+void	double_dash_args(char *argv, t_args *args)
+{
+	if (!ft_strncmp(argv, "--all", 5))
+		*args |= 1 << ARG_HIDDEN;
+	if (!ft_strncmp(argv, "--list", 6))
+		*args |= 1 << ARG_LIST;
+	if (!ft_strncmp(argv, "--recursive", 11))
+		*args |= 1 << ARG_RECURSIVE;
+	if (!ft_strncmp(argv, "--time", 6))
+		*args |= 1 << ARG_TIME;
+	if (!ft_strncmp(argv, "--reverse", 9))
+		*args |= 1 << ARG_REVERSE;
+}
+
+void	single_dash_args(char *argv, t_args *args)
+{
+	size_t	arglen;
+	size_t	i;
+
+	arglen = ft_strlen(argv);
+	i = 1;	
+	while (i < arglen)
+	{
+		if (argv[i] == 'a')
+			*args |= 1 << ARG_HIDDEN;
+		if (argv[i] == 'l')
+			*args |= 1 << ARG_LIST;
+		if (argv[i] == 'r')
+			*args |= 1 << ARG_REVERSE;
+		if (argv[i] == 't')
+			*args |= 1 << ARG_TIME;
+		if (argv[i] == 'R')
+			*args |= 1 << ARG_RECURSIVE;
+		i++;
+	}
+}
 
 t_args	check_arguments(int ac, char **av)
 {
@@ -33,17 +70,9 @@ t_args	check_arguments(int ac, char **av)
 		else
 		{
 			if (av[current_arg][1] == '-')
-				args |= get_full_arg_value(av[current_arg]);
-			if (av[current_arg][1] == 'a')
-				args |= 1 << 1;
-			if (av[current_arg][1] == 'l')
-				args |= 1 << 2;
-			if (av[current_arg][1] == 'r')
-				args |= 1 << 3;
-			if (av[current_arg][1] == 't')
-				args |= 1 << 4;
-			if (av[current_arg][1] == 'R')
-				args |= 1 << 0;
+				double_dash_args(av[current_arg], &args);
+			else
+				single_dash_args(av[current_arg], &args);
 		}
 		current_arg++;
 	}
@@ -55,5 +84,16 @@ int	main(int argc, char **argv)
 	t_args	args;
 	
 	args = check_arguments(argc, argv);
-	ft_printf("%d\n", args);
+	bool l, a, r, t, R;
+	l = args & (1 << 2);
+	a = args & (1 << 1);
+	r = args & (1 << 3);
+	t = args & (1 << 4);
+	R = args & (1 << 0);
+	ft_printf("Flags value: %d\n", args);
+	ft_printf("-l ? %d\n", l);
+	ft_printf("-r ? %d\n", r);
+	ft_printf("-a ? %d\n", a);
+	ft_printf("-t ? %d\n", t);
+	ft_printf("-R ? %d\n", R);
 }
--- src/types.h
+++ src/types.h
@@ -6,7 +6,7 @@
 /*   By: tchampio <tchampio@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 16:02:20 by tchampio          #+#    #+#             */
-/*   Updated: 2026/05/04 17:15:25 by tchampio         ###   ########.fr       */
+/*   Updated: 2026/05/04 19:10:55 by tchampio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,12 @@
                  \ -t
 */
 typedef unsigned short t_args;
+enum e_argument_flags {
+	ARG_RECURSIVE = 0,
+	ARG_HIDDEN = 1,
+	ARG_LIST = 2,
+	ARG_REVERSE = 3,
+	ARG_TIME = 4
+};
 
 #endif // TYPES_H
