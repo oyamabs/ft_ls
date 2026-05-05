61ad942d8738c1923d34ee15ea6309b49bbfbab8
Theo Champion <tchampio@c2r12p3.42lehavre.fr>
2026-05-05 17:31:33 +0200

    dev: tried to list files

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tchampio <tchampio@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 14:49:59 by tchampio          #+#    #+#             */
-/*   Updated: 2026/05/05 14:51:33 by tchampio         ###   ########.fr       */
+/*   Updated: 2026/05/05 17:24:20 by tchampio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,34 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include <sys/types.h>
+#include <dirent.h>
+
+void	recursively_explore(DIR *dp, bool *finished_exploring, struct dirent *t)
+{
+	if (*finished_exploring)
+		return ;
+	
+	t = readdir(dp);
+	while (t)
+	{
+		t = readdir(dp);
+		ft_printf("dirent: %p\ninode: %s\n",t , t->d_name);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
 	t_arguments	args;
 	init_arguments(&args, argc, argv);
 	i = 0;
+	// test opendir
+	DIR *dp = opendir(".");
+	ft_printf("Dir pointer: %p\n", dp);
+	bool explore = true;
+	struct dirent *direc = NULL;
+	recursively_explore(dp, &explore, direc);
 	while (i < args.number_of_files)
 	{
 		printf("%s ", args.filenames[i]);
@@ -31,4 +53,5 @@ int	main(int argc, char **argv)
 		i++;
 	}
 	free(args.filenames);
+	free(dp);
 }
