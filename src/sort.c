619a36937180dda54f78e57b5adce10a9c7e9ab1
Theo Champion <tchampio@student.42lehavre.fr>
2026-06-22 18:23:00 +0200

    fix: sort

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: tchampio <tchampio@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 14:02:48 by tchampio          #+#    #+#             */
-/*   Updated: 2026/06/22 12:30:29 by tchampio         ###   ########.fr       */
+/*   Updated: 2026/06/22 18:22:02 by tchampio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,31 @@
 #include <dirent.h>
 #include "utils.h"
 
-void	lower_string(char *s)
+int	ft_strcasecmp_ls(const char *s1, const char *s2)
 {
-	for (size_t i = 0; i < ft_strlen(s); i++)
-		s[i] = ft_tolower(s[i]);
+	size_t	i = 0;
+	size_t	j = 0;
+
+	// 1. Ignorer le point de départ pour les fichiers cachés
+	if (s1[i] == '.' && s1[i + 1] != '\0')
+		s1++;
+	if (s2[j] == '.' && s2[j + 1] != '\0')
+		s2++;
+
+	// 2. Comparaison classique insensible à la casse
+	while (s1[i] && s2[i])
+	{
+		if (ft_tolower((unsigned char)s1[i]) != ft_tolower((unsigned char)s2[i]))
+			return (ft_tolower((unsigned char)s1[i]) - ft_tolower((unsigned char)s2[i]));
+		i++;
+	}
+	if (ft_tolower((unsigned char)s1[i]) != ft_tolower((unsigned char)s2[i]))
+		return (ft_tolower((unsigned char)s1[i]) - ft_tolower((unsigned char)s2[i]));
+
+	// 3. LE SECRET DU VRAI LS : En cas d'égalité stricte sur les lettres (ex: aaa vs AAa)
+	// Le vrai ls fait un tri ASCII INVERSÉ (les minuscules passent AVANT les majuscules)
+	// En ASCII standard, 'a' (97) > 'A' (65). Donc s2 - s1 inverse l'ordre !
+	return (ft_strncmp(s2, s1, 256));
 }
 
 int	compare_ascii(void *content_a, void *content_b)
@@ -29,27 +50,53 @@ int	compare_ascii(void *content_a, void *content_b)
 	char	*name_a = (a->ent) ? a->ent->d_name : ft_basename(a->path);
 	char	*name_b = (b->ent) ? b->ent->d_name : ft_basename(b->path);
 
-	lower_string(name_a);
-	lower_string(name_b);
-
-	int	is_dot_a = (ft_strncmp(name_a, ".", 2) == 0 || ft_strncmp(name_a, "..", 3) == 0);
-	int	is_dot_b = (ft_strncmp(name_b, ".", 2) == 0 || ft_strncmp(name_b, "..", 3) == 0);
+	// 1. Gestion stricte et prioritaire absolue de '.' et '..'
+	int is_dot_a = (ft_strncmp(name_a, ".", 2) == 0 || ft_strncmp(name_a, "..", 3) == 0);
+	int is_dot_b = (ft_strncmp(name_b, ".", 2) == 0 || ft_strncmp(name_b, "..", 3) == 0);
 
 	if (is_dot_a && !is_dot_b)
 		return (-1);
 	if (!is_dot_a && is_dot_b)
 		return (1);
 	if (is_dot_a && is_dot_b)
-		return (ft_strlen(name_b) - ft_strlen(name_b));
+	{
+		// Force '.' à être au dessus de '..'
+		return ((int)ft_strlen(name_a) - (int)ft_strlen(name_b));
+	}
 
-	if (name_a[0] == '.' && name_a[1] != '\0')
-		name_a++;
-	if (name_b[0] == '.' && name_b[1] != '\0')
-		name_b++;
-
-	int retcode = ft_strncmp(name_a, name_b, 256);
-	return (retcode);
+	// 2. Comparaison intelligente type 'ls'
+	return (ft_strcasecmp_ls(name_a, name_b));
 }
+
+// int	compare_ascii(void *content_a, void *content_b)
+// {
+// 	t_file	*a = (t_file *)content_a;
+// 	t_file	*b = (t_file *)content_b;
+// 
+// 	char	*name_a = (a->ent) ? a->ent->d_name : ft_basename(a->path);
+// 	char	*name_b = (b->ent) ? b->ent->d_name : ft_basename(b->path);
+// 
+// 	lower_string(name_a);
+// 	lower_string(name_b);
+// 
+// 	int	is_dot_a = (ft_strncmp(name_a, ".", 2) == 0 || ft_strncmp(name_a, "..", 3) == 0);
+// 	int	is_dot_b = (ft_strncmp(name_b, ".", 2) == 0 || ft_strncmp(name_b, "..", 3) == 0);
+// 
+// 	if (is_dot_a && !is_dot_b)
+// 		return (-1);
+// 	if (!is_dot_a && is_dot_b)
+// 		return (1);
+// 	if (is_dot_a && is_dot_b)
+// 		return (ft_strlen(name_b) - ft_strlen(name_b));
+// 
+// 	if (name_a[0] == '.' && name_a[1] != '\0')
+// 		name_a++;
+// 	if (name_b[0] == '.' && name_b[1] != '\0')
+// 		name_b++;
+// 
+// 	int retcode = ft_strncmp(name_a, name_b, 256);
+// 	return (retcode);
+// }
 
 void	sort_entries_alpha(t_list *files)
 {
