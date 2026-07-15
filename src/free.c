66eb26b0a9f301372162974417fa373bd75e62d4
Theo Champion <tchampio@student.42lehavre.fr>
2026-07-15 18:04:29 +0200

    fix: double free IOT

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: tchampio <tchampio@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 14:46:37 by tchampio          #+#    #+#             */
-/*   Updated: 2026/07/15 15:55:48 by tchampio         ###   ########.fr       */
+/*   Updated: 2026/07/15 16:38:01 by tchampio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,13 @@ void	free_tree(t_file_tree *tree)
 	if (tree->statbuf)
 		free(tree->statbuf);
 	if (tree->path)
+	{
 		free(tree->path);
+	}
 	if (tree->width)
+	{
 		free(tree->width);
+	}
 	t_list *current_files = tree->files;
 	ft_lstclear(&current_files, free_file);
 	free(current_files);
@@ -47,7 +51,6 @@ void	free_tree(t_file_tree *tree)
 	while (current_branch != NULL)
 	{
 		t_file_tree *sub_tree = current_branch->content;
-		free(sub_tree->path);
 		free_tree(sub_tree);
 		current_branch = current_branch->next;
 	}
